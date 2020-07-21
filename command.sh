1f2eba4bca7fbad0abd163c76aebfdcb196dd8ed
Jackson <68029397+JacksonChen666-commits@users.noreply.github.com>
2020-07-21 20:31:34 +0200

    Commit no. 2592796

--- command.sh
+++ command.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 cd "$(dirname "$0")"
-commit() { git commit --allow-empty --no-edit -m "Commit no. $1" -q || ((c-=1)) }; c=$(git rev-list --all --count) && echo "$c"; while true ; do while [[ $(($c % 10000)) != 0 ]]; do ((c+=1)) && commit $c; done; echo $c; echo -en "Cleaning up..."; git gc --auto --force -q; echo -en "\r\033[K"; ((c+=1)); commit $c; done
+commit() { git commit --allow-empty --no-edit -m "Commit no. $1" -q && echo -n "\r$c" || ((c-=1)) }; c=$(git rev-list --all --count) && echo -n "\r$c"; while true ; do while [[ $(($c % 10000)) != 0 ]]; do ((c+=1)) && commit $c; done; echo -en "$c | Cleaning up..."; git gc --auto --force -q; echo -en "\r\033[K"; ((c+=1)); commit $c; done
